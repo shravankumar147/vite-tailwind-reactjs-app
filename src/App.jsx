09c93e3755d2dfd9b981d24a75e3379d925c94ef
Shravan Parunandula <shravanparunandula@Shravans-MBP.mshome.net>
2022-02-09 18:57:07 +0530

    base template

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Welcome, Navbar, Header, Footer } from "./components";
+
+const App = () => {
+  return (
+    <div>
+      <div>
+        <Welcome />
+        <Navbar />
+      </div>
+      <Footer />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export {default as Welcome} from './Welcome';
+export {default as Navbar} from './Navbar';
+export {default as Header} from './Header';
+export {default as Footer} from './Footer';
